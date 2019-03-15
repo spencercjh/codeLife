be364f84d28687c7a9f5bd0d2ee47f2c5814ccd0
spencercjh <caijiahao@babelchain.org>
2019-03-15 18:49:58 +0800

    26. Remove Duplicates from Sorted Array

---
+++ src/RemoveDuplicatesfromSortedArray_26.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicatesfromSortedArray_26 {
+    /**
+     * 数组是排序过的
+     * i，j双指针；遍历到有新的数字就替换掉之前相同的
+     */
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int i = 0;
+            for (int j = 1; j < nums.length; ++j) {
+                if (nums[i] != nums[j]) {
+                    nums[++i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}

94f490b7aa3a2ec0e8d2f46dfa5880e8cfab10c6
spencercjh <caijiahao@babelchain.org>
2019-03-15 19:00:03 +0800

    27. Remove Element

---
+++ src/RemoveElement_27.java
@@ -0,0 +1,16 @@
+public class RemoveElement_27 {
+    /**
+     * 双指针法，不断地把非删元素去替代要删除的元素，和第26题很相似
+     */
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int i = 0;
+            for (int j = 0; j < nums.length; ++j) {
+                if (nums[j] != val) {
+                    nums[i++] = nums[j];
+                }
+            }
+            return i;
+        }
+    }
+}

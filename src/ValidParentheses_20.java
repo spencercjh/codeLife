f53e22032e7f00c33e7681bb7ee54302c54e1735
spencercjh <caijiahao@babelchain.org>
2019-03-15 18:05:05 +0800

    20. Valid Parentheses

---
+++ src/ValidParentheses_20.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses_20 {
+    class Solution {
+        public boolean isValid(String s) {
+            Map<Character, Character> map = new HashMap<>();
+            map.put('}', '{');
+            map.put(']', '[');
+            map.put(')', '(');
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); ++i) {
+                char ch = s.charAt(i);
+                if (map.containsKey(ch)) {
+                    char top = stack.isEmpty() ? '?' : stack.pop();
+                    if (top != map.get(ch)) {
+                        return false;
+                    }
+                } else {
+                    stack.push(ch);
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+}

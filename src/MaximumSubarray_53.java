fbca1007a812a67dd5c8ebaf12c5d041c7029788
spencercjh <caijiahao@babelchain.org>
2019-03-15 23:13:25 +0800

    53. Maximum Subarray

---
+++ src/MaximumSubarray_53.java
@@ -0,0 +1,13 @@
+public class MaximumSubarray_53 {
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int sum = 0, max = nums[0];
+            for (int i = 0; i < nums.length; ++i) {
+                sum += nums[i];
+                max = Math.max(max, sum);
+                sum = sum < 0 ? 0 : sum;
+            }
+            return max;
+        }
+    }
+}

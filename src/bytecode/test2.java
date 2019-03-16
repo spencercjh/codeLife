b9be9bed0bed0d30415fe415f481fd8400e632b6
spencercjh <caijiahao@babelchain.org>
2019-03-16 11:38:47 +0800

    bytecode

---
+++ src/bytecode/test2.java
@@ -0,0 +1,64 @@
+package bytecode;
+
+import java.util.Scanner;
+
+/**
+ * @author spencercjh
+ */
+public class test2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n == 0) {
+            return;
+        }
+        while (n-- > 0) {
+            String str = in.next();
+            String origin;
+            StringBuilder edit;
+            while (true) {
+                boolean flag = false;
+                for (int i = 0; i < str.length(); ) {
+                    try {
+                        if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)) {
+                            origin = str.substring(i, i + 3);
+                            edit = new StringBuilder();
+                            edit.append(str.charAt(i)).append(str.charAt(i + 1));
+                            str = str.replace(origin, edit);
+                            i += 3;
+                            flag = true;
+                        }
+                        if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i + 2) == str.charAt(i + 3) && str.charAt(i) != str.charAt(i + 2)) {
+                            origin = str.substring(i, i + 4);
+                            edit = new StringBuilder();
+                            edit.append(str.charAt(i)).append(str.charAt(i + 1)).append(str.charAt(i + 2));
+                            str = str.replace(origin, edit);
+                            i += 4;
+                            flag = true;
+                        }
+                        if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i + 2) == str.charAt(i + 3) && str.charAt(i) != str.charAt(i + 2) && str.charAt(i + 4) == str.charAt(i + 5) && str.charAt(i + 2) != str.charAt(i + 4)) {
+                            origin = str.substring(i, i + 6);
+                            edit = new StringBuilder();
+                            edit.append(str.charAt(i)).append(str.charAt(i + 1)).append(str.charAt(i + 2)).append(str.charAt(i + 4)).append(str.charAt(i + 5));
+                            str = str.replace(origin, edit);
+                            i += 6;
+                            flag = true;
+                        }
+                    } catch (Exception e) {
+                        break;
+                    }
+                    if (!flag) {
+                        ++i;
+                    }
+                    if (i + 2 >= str.length()) {
+                        break;
+                    }
+                }
+                if (!flag) {
+                    break;
+                }
+            }
+            System.out.println(str);
+        }
+    }
+}
---
+++ src/bytecode/test3.java
@@ -0,0 +1,41 @@
+package bytecode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author spencercjh
+ */
+public class test3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        while (n-- > 0) {
+            int m = in.nextInt();
+            int[] scores = new int[m];
+            int[] prices = new int[m];
+            for (int i = 0; i < m; ++i) {
+                scores[i] = in.nextInt();
+                prices[i] = 1;
+            }
+            for (int i = 1; i < m; ++i) {
+                if (scores[i] > scores[i - 1] && prices[i] <= prices[i - 1]) {
+                    prices[i] = prices[i - 1] + 1;
+                } else if (scores[i] < scores[i - 1] && prices[i] >= prices[i - 1]) {
+                    prices[i - 1] = prices[i] + 1;
+                }
+                if (scores[i] > scores[(i + 1) % m] && prices[i] <= prices[(i + 1) % m]) {
+                    prices[i] = prices[(i + 1) % m] + 1;
+                } else if (scores[i] < scores[(i + 1) % m] && prices[i] >= prices[(i + 1) % m]) {
+                    prices[(i + 1) % m] = prices[i] + 1;
+                }
+            }
+            int ans = 0;
+            for (int i = 0; i < m; ++i) {
+                ans += prices[i];
+            }
+            System.out.println(Arrays.toString(prices));
+            System.out.println(ans);
+        }
+    }
+}

dcbf09455ea2f5b051e3c210233692b57c614e8f
spencercjh <caijiahao@babelchain.org>
2019-03-20 13:27:42 +0800

    237. Delete Node in a Linked List

---
+++ src/DeleteNodeinaLinkedList_237.java
@@ -0,0 +1,20 @@
+public class DeleteNodeinaLinkedList_237 {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}

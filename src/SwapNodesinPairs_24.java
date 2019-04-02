20741e0f2c1c1ae9d147ac3d100c8d6ccf733b42
spencercjh <caijiahao@babelchain.org>
2019-04-03 00:12:05 +0800

    24. Swap Nodes in Pairs

---
+++ src/SwapNodesinPairs_24.java
@@ -0,0 +1,33 @@
+/**
+ * @author spencercjh
+ */
+public class SwapNodesinPairs_24 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode next = head.next;
+            head.next = swapPairs(next.next);
+            next.next = head;
+            return next;
+        }
+    }
+}

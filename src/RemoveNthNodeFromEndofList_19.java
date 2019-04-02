7d34778088bb7cf0491b7e58a4dfcf6be40a7643
spencercjh <caijiahao@babelchain.org>
2019-04-02 22:54:01 +0800

    19. Remove Nth Node From End of List

---
+++ src/RemoveNthNodeFromEndofList_19.java
@@ -0,0 +1,58 @@
+/**
+ * @author spencercjh
+ */
+public class RemoveNthNodeFromEndofList_19 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution1 {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0), first = head;
+            dummy.next = head;
+            int length = 0;
+            while (first != null) {
+                first = first.next;
+                length++;
+            }
+            length -= n;
+            first = dummy;
+            while (length > 0) {
+                length--;
+                first = first.next;
+            }
+            first.next = first.next.next;
+            return dummy.next;
+        }
+    }
+
+    class Solution2 {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode first = dummy, second = dummy;
+            for (int i = 1; i <= n + 1; i++) {
+                first = first.next;
+            }
+            while (first != null) {
+                first = first.next;
+                second = second.next;
+            }
+            second.next = second.next.next;
+            return dummy.next;
+        }
+    }
+}

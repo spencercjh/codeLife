bcb2e1d7e68941cb247604832c2499e93a530ef3
spencercjh <caijiahao@babelchain.org>
2019-04-02 19:45:23 +0800

    15. 3Sum

--- src/ThreeSum_15.java
+++ src/ThreeSum_15.java
@@ -3,30 +3,25 @@ import java.util.*;
 public class ThreeSum_15 {
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> lists = new ArrayList<>();
-            Set<List<Integer>> hashSet = new HashSet<>();
             Arrays.sort(nums);
-            int len = nums.length;
-            for (int time = 0; time <= len - 3; time++) {
-                int left = time + 1, right = len - 1;
-                while (left < right) {
-                    int sum = nums[time] + nums[left] + nums[right];
-                    if (sum < 0) {
-                        left++;
-                    } else if (sum > 0) {
+            Set<List<Integer>> answer = new HashSet<>();
+            int n = nums.length;
+            for (int left = 0; left <= n - 3; left++) {
+                int middle = left + 1, right = n - 1;
+                while (middle < right) {
+                    int sum = nums[left] + nums[middle] + nums[right];
+                    if (sum > 0) {
                         right--;
+                    } else if (sum < 0) {
+                        middle++;
                     } else {
-                        List<Integer> answer = Arrays.asList(nums[time], nums[left], nums[right]);
-                        hashSet.add(answer);
-                        left++;
+                        answer.add(Arrays.asList(nums[left], nums[middle], nums[right]));
+                        middle++;
                         right--;
                     }
                 }
             }
-            if (!hashSet.isEmpty()) {
-                lists.addAll(hashSet);
-            }
-            return lists;
+            return new ArrayList<>(answer);
         }
     }
 }

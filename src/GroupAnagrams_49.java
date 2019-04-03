7a201b39ff869fa2faa79e748477e6292074e3f3
spencercjh <caijiahao@babelchain.org>
2019-04-03 23:24:28 +0800

    49. Group Anagrams

---
+++ src/GroupAnagrams_49.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+ * @author spencercjh
+ */
+public class GroupAnagrams_49 {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> answerMap = new HashMap<>();
+            for (String str : strs) {
+                int[] counts = new int[26];
+                for (char ch : str.toCharArray()) {
+                    counts[ch - 'a']++;
+                }
+                String key = Arrays.toString(counts);
+                if (!answerMap.containsKey(key)) {
+                    answerMap.put(key, new ArrayList<>());
+                }
+                answerMap.get(key).add(str);
+            }
+            return new ArrayList<>(answerMap.values());
+        }
+    }
+}

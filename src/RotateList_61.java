c70986bb7da79d70244257530fe8f0fd32474018
spencercjh <shouspencercjh@foxmail.com>
2019-04-09 00:20:57 +0800

    61. Rotate List

---
+++ src/RotateList_61.java
@@ -0,0 +1,63 @@
+/**
+ * 思路from曾文浩:说是循环旋转，但其实本质上是将尾部向前数第K个元素作为头，原来的头接到原来的尾上
+ *
+ * @author SpencerCJH
+ * @date 2019/4/8 18:45
+ */
+public class RotateList_61 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public ListNode rotateRight(ListNode head, int k) {
+            if (null == head) {
+                return null;
+            }
+            ListNode tail = null, newTail = null, newHead = null, current = head;
+            int newTailIndex = 0, length = 0;
+            while (null != current) {
+                length++;
+                current = current.next;
+            }
+            k = k >= length ? k % length : k;
+            if (k == 0 || head.next == null) {
+                return head;
+            }
+            current = head;
+            while (null != current) {
+                newTailIndex++;
+                if (newTailIndex == length - k) {
+                    newTail = current;
+                    newHead = current.next;
+                }
+                if (current.next == null) {
+                    tail = current;
+                }
+                current = current.next;
+            }
+            assert newHead != null;
+            if (newHead != tail) {
+                tail.next = head;
+                newTail.next = null;
+            } else {
+                newTail.next = null;
+                newHead.next = head;
+            }
+            return newHead;
+        }
+    }
+}

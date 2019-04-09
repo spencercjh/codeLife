1120aacc73bf01f4744bef4ced2d0ad336ee33bb
spencercjh <shouspencercjh@foxmail.com>
2019-04-09 11:30:57 +0800

    70. Climbing Stairs

---
+++ src/ClimbingStairs_70.java
@@ -0,0 +1,19 @@
+/**
+ * @author SpencerCJH
+ * @date 2019/4/9 11:26
+ */
+public class ClimbingStairs_70 {
+    class Solution {
+        public int climbStairs(int n) {
+            int[] f = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                if (i == 0 || i == 1) {
+                    f[i] = 1;
+                } else {
+                    f[i] = f[i - 1] + f[i - 2];
+                }
+            }
+            return f[n];
+        }
+    }
+}

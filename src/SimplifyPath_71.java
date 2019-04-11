3399e534853d09d2ee0d030476bd55bb6299241a
spencercjh <shouspencercjh@foxmail.com>
2019-04-11 16:07:56 +0800

    71. Simplify Path

---
+++ src/SimplifyPath_71.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+/**
+ * @author : SpencerCJH
+ * @date : 2019/4/11 15:52
+ */
+public class SimplifyPath_71 {
+
+    class Solution {
+        public String simplifyPath(String path) {
+            if (null == path || path.length() == 0) {
+                return path;
+            }
+            String[] strings = path.split("/");
+            Stack<String> stack = new Stack<>();
+            for (String s : strings) {
+                if ("".equals(s) || ".".equals(s)) {
+                    continue;
+                } else if ("..".equals(s)) {
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                } else {
+                    stack.push(s);
+                }
+            }
+            if (stack.isEmpty()) {
+                return "/";
+            }
+            StringBuilder sb = new StringBuilder();
+            while (!stack.isEmpty()) {
+                sb.insert(0, "/" + stack.pop());
+            }
+            return sb.toString();
+        }
+    }
+}

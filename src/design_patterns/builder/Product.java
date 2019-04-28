961334ea695f2d8ca4d33f313f56a1902b7da964
spencercjh <shouspencercjh@foxmail.com>
2019-04-28 21:40:05 +0800

    建造者模式

---
+++ src/design_patterns/builder/Product.java
@@ -0,0 +1,32 @@
+package design_patterns.builder;
+
+/**
+ * @author SpencerCJH
+ * @date 2019/4/28 21:25
+ */
+public class Product {
+    private int part;
+    private String model;
+
+    public int getPart() {
+        return part;
+    }
+
+    public Product setPart(int part) {
+        this.part = part;
+        return this;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Product setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public void doSomething() {
+        System.out.println("Product");
+    }
+}

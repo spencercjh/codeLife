8f99283837ee11da7b03b303b1dcde2c13f8c958
spencercjh <shouspencercjh@foxmail.com>
2019-04-28 21:12:45 +0800

    模板方法模式

---
+++ src/design_patterns/template_method/Client.java
@@ -0,0 +1,14 @@
+package design_patterns.template_method;
+
+/**
+ * @author SpencerCJH
+ * @date 2019/4/28 21:09
+ */
+public class Client {
+    public static void main(String[] args) {
+        AbstractClass class1 = new ConcreteClass1();
+        AbstractClass class2 = new ConcreteClass2();
+        class1.templateMethod();
+        class2.templateMethod();
+    }
+}

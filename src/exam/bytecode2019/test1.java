f85072b3b8a9dbba4e6bd3f237e6a81bab7a3607
spencercjh <shouspencercjh@foxmail.com>
2019-06-21 11:31:18 +0800

    面经3则

--- src/exam/bytecode2019/test1.java
+++ src/exam/bytecode2019/test1.java
@@ -3,6 +3,9 @@ package exam.bytecode2019;
 import java.util.Scanner;
 
 /**
+ * Z国的货币系统包含面值1元、4元、16元、64元共计4种硬币，以及面值1024元的纸币。现在小Y使用1024元的纸币购买了一件价值为的商品，请问最少他会收到多少硬币？
+ * https://www.nowcoder.com/question/next?pid=16516564&qid=362294&tid=24564608
+ *
  * @author spencercjh
  */
 public class test1 {

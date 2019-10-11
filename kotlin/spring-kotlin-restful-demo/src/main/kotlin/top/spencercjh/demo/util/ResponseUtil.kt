1cdcce740101d6715ea5f8735f31df1016f52cbd
spencercjh <shouspencercjh@foxmail.com>
2019-10-12 00:02:32 +0800

    Implemented a ResponseTool class for ResponseEntity

---
+++ kotlin/spring-kotlin-restful-demo/src/main/kotlin/top/spencercjh/demo/util/ResponseUtil.kt
@@ -0,0 +1,74 @@
+package top.spencercjh.demo.util
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import top.spencercjh.demo.util.ResponseUtil.Result
+import java.sql.Timestamp
+
+/**
+ * package [ResponseEntity] as a util. Its body is a package class [Result] with [Result.Builder] pattern
+ * @author spencer
+ */
+object ResponseUtil {
+
+    fun <A> success(code: Int = HttpStatus.OK.value(), message: String = "Request success", body: A? = null): ResponseEntity<Result<A>> {
+        return ResponseEntity(
+                Result.Builder<A>().code(code).status(true).message(message).body(body).build(),
+                HttpStatus.OK
+        )
+    }
+
+    fun <A> failed(code: Int = HttpStatus.INTERNAL_SERVER_ERROR.value(), message: String = "Request failed"): ResponseEntity<Result<A>> {
+        return ResponseEntity(
+                Result.Builder<A>().code(code).status(false).message(message).build(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        )
+    }
+
+    class Result<T>(val code: Int?,
+                    val message: String?,
+                    val status: Boolean?,
+                    val body: T? = null,
+                    val timestamp: Timestamp = Timestamp(System.currentTimeMillis())) {
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Result<*>
+
+            if (code != other.code) return false
+            if (message != other.message) return false
+            if (status != other.status) return false
+            if (body != other.body) return false
+            if (timestamp != other.timestamp) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = code ?: 0
+            result = 31 * result + (message?.hashCode() ?: 0)
+            result = 31 * result + (status?.hashCode() ?: 0)
+            result = 31 * result + (body?.hashCode() ?: 0)
+            result = 31 * result + timestamp.hashCode()
+            return result
+        }
+
+        override fun toString(): String {
+            return "Result(code=$code, message=$message, status=$status, body=$body, timestamp=$timestamp)"
+        }
+
+        data class Builder<T>(var code: Int? = null,
+                              var message: String? = null,
+                              var status: Boolean? = null,
+                              var body: T? = null
+        ) {
+            fun code(code: Int) = apply { this.code = code }
+            fun message(message: String) = apply { this.message = message }
+            fun status(status: Boolean) = apply { this.status = status }
+            fun body(body: T?) = apply { this.body = body }
+            fun build() = Result(code, message, status, body)
+        }
+    }
+}
---
+++ kotlin/spring-kotlin-restful-demo/src/test/kotlin/top/spencercjh/demo/util/ResponseUtilTest.kt
@@ -0,0 +1,76 @@
+package top.spencercjh.demo.util
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import top.spencercjh.demo.entity.Clazz
+import top.spencercjh.demo.entity.Student
+
+/**
+ * test ResponseUtil use Junit5
+ * @author spencer
+ * @see ResponseUtil
+ */
+internal class ResponseUtilTest {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val student: Student = Student(name = "蔡佳昊",
+            clazz = Clazz(name = "test"),
+            sex = Student.Sex.Male,
+            phone = RandomUtil.getRandomPhone(),
+            email = RandomUtil.getRandomEmail())
+
+    @Test
+    fun success() {
+        // custom parameter test
+        var expect = ResponseUtil.Result(code = HttpStatus.ACCEPTED.value(), status = true, message = "Test success", body = student)
+        // different timeStamp
+        Thread.sleep(1000)
+        var responseEntity = ResponseUtil.success(code = HttpStatus.ACCEPTED.value(), message = "Test success", body = student)
+        logger.debug(responseEntity.toString())
+        assertEquals(expect.code, responseEntity.body!!.code)
+        assertEquals(expect.message, responseEntity.body!!.message)
+        assertEquals(expect.status, responseEntity.body!!.status)
+        assertEquals(expect.body, responseEntity.body!!.body)
+        assertNotEquals(expect, responseEntity.body)
+        // default parameter test
+        expect = ResponseUtil.Result(code = HttpStatus.OK.value(), message = "Request success", status = true)
+        // different timeStamp
+        Thread.sleep(1000)
+        responseEntity = ResponseUtil.success()
+        logger.debug(responseEntity.toString())
+        assertEquals(expect.code, responseEntity.body!!.code)
+        assertEquals(expect.message, responseEntity.body!!.message)
+        assertEquals(expect.status, responseEntity.body!!.status)
+        assertEquals(expect.body, responseEntity.body!!.body)
+        assertTrue(responseEntity.body!!.body == null)
+        assertNotEquals(expect, responseEntity.body)
+    }
+
+    @Test
+    fun fail() {
+        // custom parameter test
+        var expect = ResponseUtil.Result<Any>(code = HttpStatus.SERVICE_UNAVAILABLE.value(), status = false, message = "Test failed")
+        // different timeStamp
+        Thread.sleep(1000)
+        var responseEntity = ResponseUtil.failed<Any>(code = HttpStatus.SERVICE_UNAVAILABLE.value(), message = "Test failed")
+        logger.debug(responseEntity.toString())
+        assertEquals(expect.code, responseEntity.body!!.code)
+        assertEquals(expect.message, responseEntity.body!!.message)
+        assertEquals(expect.status, responseEntity.body!!.status)
+        assertEquals(expect.body, responseEntity.body!!.body)
+        assertNotEquals(expect, responseEntity.body)
+        // default parameter test
+        expect = ResponseUtil.Result(code = HttpStatus.INTERNAL_SERVER_ERROR.value(), message = "Request failed", status = false)
+        // different timeStamp
+        Thread.sleep(1000)
+        responseEntity = ResponseUtil.failed()
+        logger.debug(responseEntity.toString())
+        assertEquals(expect.code, responseEntity.body!!.code)
+        assertEquals(expect.message, responseEntity.body!!.message)
+        assertEquals(expect.status, responseEntity.body!!.status)
+        assertEquals(expect.body, responseEntity.body!!.body)
+        assertTrue(responseEntity.body!!.body == null)
+        assertNotEquals(expect, responseEntity.body)
+    }
+}

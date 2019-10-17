1b82218a5aa03efcf15c7579a1890f6669b93d68
spencercjh <fixed-term.spencer.cai@cn.bosch.com>
2019-10-17 16:19:51 +0800

    add a module:kotlin-feature

---
+++ kotlin/kotlin-feature/README.md
@@ -0,0 +1,3 @@
+# Kotlin Study
+
+我将着重介绍Kotlin中和Java不一样的语言特性。参考：[kotlin 中国](https://www.kotlincn.net/docs/reference/)
---
+++ kotlin/kotlin-feature/src/DataClassTests.kt
@@ -0,0 +1,20 @@
+package top.spencercjh.demo
+
+data class User(val name: String = "testName", val age: Int = 22) {
+    // data class类体中的属性将不会有component()函数，不会在toString()、 equals()、 hashCode() 以及 copy()中出现
+    var innerValue: String = "123"
+
+    val readOnlyInnerValue: String = "readOnly"
+}
+
+val user: User = User()
+
+fun main(args: Array<String>) {
+    println(user)
+    user.innerValue = "456"
+    println(user.innerValue)
+    println(user.readOnlyInnerValue)
+    println(user.hashCode())
+}
+
+

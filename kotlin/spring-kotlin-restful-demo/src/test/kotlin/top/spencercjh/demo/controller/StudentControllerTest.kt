ace62cd1c1473651e90921c02d925ae5de0c6d7b
spencercjh <fixed-term.spencer.cai@cn.bosch.com>
2019-10-17 10:52:58 +0800

    complete api unit test.

--- kotlin/spring-kotlin-restful-demo/src/test/kotlin/top/spencercjh/demo/controller/StudentControllerTest.kt
+++ kotlin/spring-kotlin-restful-demo/src/test/kotlin/top/spencercjh/demo/controller/StudentControllerTest.kt
@@ -1,16 +1,19 @@
 package top.spencercjh.demo.controller
 
+import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import top.spencercjh.demo.SpringKotlinRestfulDemoApplication.Constant
+import top.spencercjh.demo.entity.Student
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
@@ -23,26 +26,54 @@ internal class StudentControllerTest {
     fun findAllStudents() {
         mockMvc.perform(get("$prefix/students"))
                 .andExpect(status().isOk)
-                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.status").value(true))
                 .andExpect(jsonPath("$.message").value("Request success"))
                 .andExpect(jsonPath("$.body").exists())
+                .andExpect(jsonPath("$.body.content").exists())
+                .andExpect(jsonPath("$.body.content").isArray)
+                .andExpect(jsonPath("$.body.content", hasSize<Student>(Constant.DEFAULT_PAGE_SIZE)))
+                .andExpect(jsonPath("$.body.pageable").isMap)
+                .andExpect(jsonPath("$.body.sort").isMap)
+                .andDo(MockMvcResultHandlers.print())
     }
 
     @Test
     fun findStudentsByClass() {
-        mockMvc.perform(get("$prefix/classes/1/students"))
+        mockMvc.perform(get("$prefix/classes/2/students").param("size", (Constant.MOCK_STUDENT_AMOUNT * 2).toString()))
                 .andExpect(status().isOk)
-                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.status").value(true))
                 .andExpect(jsonPath("$.message").value("Request success"))
                 .andExpect(jsonPath("$.body").exists())
+                .andExpect(jsonPath("$.body.content").exists())
+                .andExpect(jsonPath("$.body.content").isArray)
+                .andExpect(jsonPath("$.body.content", hasSize<Student>(Constant.MOCK_STUDENT_AMOUNT * 2)))
+                .andExpect(jsonPath("$.body.content[0].clazz.id").value(2))
+                .andExpect(jsonPath("$.body.content[0].clazz.name").value(Constant.CLASS_NAME_TWO))
+                .andExpect(jsonPath("$.body.pageable").isMap)
+                .andExpect(jsonPath("$.body.sort").isMap)
+                .andDo(MockMvcResultHandlers.print())
     }
 
     @Test
     fun findStudentByClassAndStudentId() {
         mockMvc.perform(get("$prefix/classes/2/students/91"))
                 .andExpect(status().isOk)
-                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.code").value(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.status").value(true))
                 .andExpect(jsonPath("$.message").value("Request success"))
                 .andExpect(jsonPath("$.body").exists())
+                .andExpect(jsonPath("$.body").isMap)
+                .andExpect(jsonPath("$.body.name").value("蔡佳昊"))
+                .andExpect(jsonPath("$.body.sex").value(Student.Sex.Male.name))
+                .andExpect(jsonPath("$.body.id").value(91))
+                .andExpect(jsonPath("$.body.clazz").isMap)
+                .andExpect(jsonPath("$.body.clazz.id").value(2))
+                .andExpect(jsonPath("$.body.clazz.name").value(Constant.CLASS_NAME_TWO))
+                .andDo(MockMvcResultHandlers.print())
     }
 }
